1e99af44c67d8dc9ff90bd9fe54e077ed10ddee4
andy <andydavies2020@gmail.com>
2018-05-26 13:30:05 +0300

    tests: add attribute to only run tests if Consul is available

---
+++ src/Config.Consul.Tests/RequiresConsulFactAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Consul;
+using Xunit;
+
+namespace Config.Consul.Tests
+{
+	public class RequiresConsulFactAttribute : FactAttribute
+	{
+		private static readonly Lazy<bool> IsAvailable = new Lazy<bool>(() =>
+		{
+			using (var client = new ConsulClient())
+			{
+				try
+				{
+					client.KV.List("").Wait();
+					return true;
+				}
+				catch (Exception exception)
+				{
+					return false;
+				}
+			}
+		});
+
+		public override string Skip
+		{
+			get => IsAvailable.Value ? string.Empty : "Consul not detected";
+			set { }
+		}
+	}
+}
